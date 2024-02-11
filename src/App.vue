ec65b781a3e2fae8ec881c4e90cc4e1790912bdf
FirdanAja <firdanfauzan5@gmail.com>
2024-02-11 22:54:25 +0700

    search page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,172 @@
 <template>
-  <header>
-    <h1 class="font-bold text-3xl">Lets Build Something AWESOME!</h1>
-  </header>
+  <div class="menu">
+    <div class="kategori">
+      <h3>Jenis Kategori</h3>
+      <a href="">Object wisata</a><br />
+      <a href="">Aktivasi Luar Ruangan</a><br />
+      <a href="">Konser & Pertunjukan</a><br />
+      <a href="">Makanan & Minuman</a><br />
+      <a href="">Acara</a><br />
+    </div>
+    <div class="objectWisata">
+      <h3>Jenis Objek Wisata</h3>
+      <a href="">Tur</a><br />
+      <a href="">Tur dengan Perahu</a><br />
+      <a href="">Wisata Alam</a><br />
+      <a href="">Hiburan</a><br />
+      <a href="">Museum</a><br />
+    </div>
+    <div class="penilaian">
+      <h3>Penilaian Wisatawan</h3>
+      <a href="">
+        <input type="checkbox" />
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+      </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" />
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span> & ke atas
+      </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" />
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span> & keatas
+      </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" />
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span> & ke atas
+      </a>
+    </div>
+    <div class="Kawasan">
+      <h3>Kawasan</h3>
+      <a href=""> <input type="checkbox" /> Kuningan </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" /> Pangandaran
+      </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" /> Majalengka
+      </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" /> Bandung
+      </a>
+    </div>
+    <div class="cocok">
+      <h3>Cocok Untuk</h3>
+      <a href=""> <input type="checkbox" /> Cocok Untuk Pasangan </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" /> Terjangkau
+      </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" /> Cocok Untuk Anak
+      </a>
+      <a href=""
+        ><br />
+        <input type="checkbox" /> Cocok Untuk Keluarga
+      </a>
+    </div>
+  </div>
+  <b><p>9.000+ tempat yang diurut berdasarkan favorit wisatawan</p></b>
+  <div class="grid">
+    <a href=""><img src="./assets/img/a1.jpg" alt="" /></a>
+    <div class="text-box">
+      <b><a href="">1. Nama Wisata</a></b
+      ><br />
+      <a href="">
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <a href="">16.623</a><br />
+        <p>Taman Air</p>
+        <p>Sekarang Buka</p>
+        <a href=""><button style="font-weight: bold">Lihat Tiket</button></a>
+        <a href=""><button style="font-weight: bold">Lihat Tur</button></a>
+      </a>
+    </div>
+
+    <div style="padding-top: 80px">
+      <a href=""><img src="./assets/img/b1.jpg" alt="" /></a>
+      <div class="text-box">
+        <b><a href="">2. Nama Wisata</a></b
+        ><br />
+        <a href="">
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <a href="">10.623</a><br />
+          <p>Taman Wisata</p>
+          <p>Sekarang Buka</p>
+          <a href=""><button style="font-weight: bold">Lihat Tiket</button></a>
+          <a href=""><button style="font-weight: bold">Lihat Tur</button></a>
+        </a>
+      </div>
+    </div>
+
+    <div style="padding-top: 80px">
+      <a href=""><img src="./assets/img/c1.jpg" alt="" /></a>
+      <div class="text-box">
+        <b><a href="">3. Nama Wisata</a></b
+        ><br />
+        <a href="">
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <a href="">9.623</a><br />
+          <p>Taman Wisata</p>
+          <p>Sekarang Buka</p>
+          <a href=""><button style="font-weight: bold">Lihat Tiket</button></a>
+          <a href=""><button style="font-weight: bold">Lihat Tur</button></a>
+        </a>
+      </div>
+    </div>
+
+    <div style="padding-top: 70px">
+      <a href=""><img src="./assets/img/d1.jpg" alt="" /></a>
+      <div class="text-box">
+        <b><a href="">4. Nama Wisata</a></b
+        ><br />
+        <a href="">
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <a href="">13.623</a><br />
+          <p>Taman Wisata</p>
+          <p>Sekarang Buka</p>
+          <a href=""><button style="font-weight: bold">Lihat Tiket</button></a>
+          <a href=""><button style="font-weight: bold">Lihat Tur</button></a>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +174,36 @@ export default {};
 </script>
 
 <style>
+body {
+  font-family: "sen", sans-serif;
+}
+.menu {
+  margin-left: 100px;
+  float: left;
+}
+a {
+  text-decoration: none;
+  color: black;
+  line-height: 30px;
+}
+.checked {
+  color: green;
+}
+img {
+  max-width: 300px;
+  float: left;
+  margin-right: 20px;
+  margin-left: 40px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 400ms;
+}
+button {
+  background-color: rgb(239, 215, 0);
+  color: black;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+}
 </style>
