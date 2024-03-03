09d21ce684c4f007a7d04d53414878b5214dd60d
ilhamhafidz404 <xxspanzie@gmail.com>
2024-03-03 10:41:15 +0700

    Fix deploy to vercel

--- src/views/News.vue
+++ src/views/News.vue
@@ -313,7 +313,7 @@
       </div>
       <div class="xl:block hidden">
         <img
-          src="./../assets/img/a1.jpg"
+          src="https://images.unsplash.com/photo-1509861336350-714f2cf2af44?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="kabarBudayaBanner"
           class="rounded"
         />
